15ed8043652950a25338653e7eff2a6175148e61
Hans <hansie211@hotmail.com>
2020-04-28 15:48:21 +0200

    Include placeholder for api key

---
+++ WalkingDinner/Mollie/MollieAPI.key.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WalkingDinner.Mollie {
+    public static partial class MollieAPI {
+
+#warning EMPTY KEY
+
+        public static readonly string Key       = "";
+        public static readonly string Username  = "";
+        public static readonly string Password  = "";
+    }
+}
